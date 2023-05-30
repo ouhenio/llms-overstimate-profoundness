66f4919133ee1ac4a2c267c282c3fe42c5a2e6b1
tvergara <tomvergara@uc.cl>
2023-05-29 23:03:14 -0400

    feat(): add gpt as a sentence cleanup method

--- generate_bs_sentences.py
+++ generate_bs_sentences.py
@@ -3,16 +3,26 @@ import pandas as pd
 import numpy as np
 import spacy
 import random
+import openai
+import guidance
 from tqdm import tqdm
 
+OPENAI_MODEL = 'gpt-3.5-turbo'
 nlp = spacy.load("en_core_web_lg")
 
 def generate_bs_sentences(
     input_file='data/BS.xlsx',
+    openai_key=None,
     output_file='data/BS_generated.xlsx',
 ):
     bs_sentences = pd.read_excel(input_file, header=None)[0]
     sentences = list(map(lambda sentence: create_sentence_variation(sentence), tqdm(bs_sentences)))
+
+    if openai_key:
+        openai.api_key = openai_key
+        sentences = cleanup_sentences(sentences)
+    else:
+        print('No OpenAI key provided, skipping cleanup')
 
     save_sentences(sentences, output_file)
 
@@ -38,6 +48,26 @@ def most_similar_words(word, n=10):
     )
     words = [nlp.vocab.strings[string] for string in vector_words[0][0]]
     return words
+
+
+def cleanup_sentences(sentences):
+    gpt = guidance.llms.OpenAI(OPENAI_MODEL)
+    rewrite_sentence = guidance('''
+    {{#user~}}
+    Fix any inconsistent uppercase, spaces or punctuation of the following sentence:
+
+    {{sentence}}
+    {{~/user}}
+
+    {{#assistant~}}
+    {{gen 'answer'}}
+    {{~/assistant}}
+    ''', llm=gpt)
+
+    new_sentences = list(map(
+        lambda sentence: rewrite_sentence(sentence=sentence)['answer'], sentences
+    ))
+    return new_sentences
 
 def save_sentences(sentences, output_file):
     df = pd.DataFrame(sentences)
