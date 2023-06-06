74011b8b1d96e0d7117f9b26e89062b8501e2837
Eugenio Herrera-Berg <eiherrera@uc.cl>
2023-06-06 16:17:26 -0600

    feat: improve recovery system

--- rate_sentences.py
+++ rate_sentences.py
@@ -1,6 +1,8 @@
 import json
 import os
 import random
+import time
+import traceback
 
 import fire
 import guidance
@@ -35,23 +37,23 @@ def assign_dataset_to_sentences(sentences: list, dataset: str) -> list:
     return [{"dataset": dataset, "sentence": sentence} for sentence in sentences]
 
 
-def evaluate_sentences_separated(
+def evaluate_sentence(
     evaluation_prompt: str,
-    sentences: list,
+    sentence: str,
     temperature: int,
     model: str = "gpt-3.5",
-) -> list:
+) -> int:
     gpt = guidance.llms.OpenAI(OPENAI_MODELS[model], caching=False)
-    evaluate_sentence = guidance(
+    evaluate_sentence_ = guidance(
         """
     {{#user~}}
     {{evaluation_prompt}}
 
     Provide your rating as a simple numerical value, without any accompanying text.
-    
+
     Sentence:
     ----
-    {{sentence.sentence}}
+    {{sentence}}
 
 
     {{~/user}}
@@ -63,23 +65,18 @@ def evaluate_sentences_separated(
         llm=gpt,
     )
 
-    scores = list(
-        map(
-            lambda sentence: int(
-                evaluate_sentence(
-                    evaluation_prompt=evaluation_prompt,
-                    sentence=sentence,
-                    temperature=temperature,
-                )["score"]
-            ),
-            tqdm(sentences),
-        )
+    score = int(
+        evaluate_sentence_(
+            evaluation_prompt=evaluation_prompt,
+            sentence=sentence,
+            temperature=temperature,
+        )["score"]
     )
 
-    return scores
+    return score
 
 
-def assign_experiments_values(
+def assign_experiment_values(
     score,
     trial,
     temperature,
@@ -107,7 +104,7 @@ def load_last_state():
         last_subject = last_state.get("subject", 0)
         last_temperature_type = last_state.get("temperature_type", None)
         last_evaluation_type = last_state.get("evaluation_type", None)
-        last_trial_counter = last_state.get('trial_counter', 0)
+        last_trial_counter = last_state.get("trial_counter", 0)
     except (FileNotFoundError, json.JSONDecodeError):
         last_subject = 0
         last_temperature_type = None
@@ -141,88 +138,153 @@ def rate_sentences(
 
     NUM_SENTENCES = len(all_sentences)
 
-    df = pd.DataFrame()
-
-    last_subject, last_temperature_type, last_evaluation_type, last_trial_counter = load_last_state()
+    (
+        last_subject,
+        last_temperature_type,
+        last_evaluation_type,
+        last_trial_counter,
+    ) = load_last_state()
 
     # current state variable handlers
     current_subject = last_subject
     current_temperature_type = last_temperature_type
     current_evaluation_type = last_evaluation_type
     current_trial_counter = last_trial_counter
+    current_i = last_trial_counter % NUM_SENTENCES
 
     skip_temperature = last_temperature_type is not None
     skip_evaluation = last_evaluation_type is not None
 
-    for subject in tqdm(range(last_subject, NUM_SUBJECTS), desc="Subjects"):
+    df = (
+        pd.read_csv(output_file)
+        if last_temperature_type is not None
+        else pd.DataFrame()
+    )
+
+    TOTAL_TRIALS = NUM_SUBJECTS * 2 * 10 * NUM_SENTENCES
+    progress_bar = tqdm(range(TOTAL_TRIALS))
+    if current_trial_counter > 0:
+        progress_bar.update(current_trial_counter + 1)
+
+    for subject in range(last_subject, NUM_SUBJECTS):
         trial_counter = last_trial_counter if subject == last_subject else 0
-        for temperature_type, temperature_value in tqdm(
-            TEMPERATURES.items(), desc="Temperatures"
-        ):  # 2 temperature types
+        for (
+            temperature_type,
+            temperature_value,
+        ) in TEMPERATURES.items():  # 2 temperature types
             if skip_temperature and temperature_type != last_temperature_type:
                 continue
             skip_temperature = False
-            for evaluation_type, evaluation_prompt in tqdm(
-                EVALUATION_PROMPTS_DICT.items(), desc="Evaluations"
-            ):  # 10 evaluation types
+            for (
+                evaluation_type,
+                evaluation_prompt,
+            ) in EVALUATION_PROMPTS_DICT.items():  # 10 evaluation types
                 if skip_evaluation and evaluation_type != last_evaluation_type:
                     continue
                 skip_evaluation = False
-                try:
-                    evaluation_scores = evaluate_sentences_separated(
-                        evaluation_prompt=evaluation_prompt,
-                        sentences=all_sentences,
-                        temperature=temperature_value,
-                        model=model,
-                    )
-                except Exception as e:
-                    # Save the current state before terminating
-                    with open("last_successful_state.json", "w") as f:
-                        json.dump(
-                            {
-                                "subject": current_subject,
-                                "temperature_type": current_temperature_type,
-                                "evaluation_type": current_evaluation_type,
-                                'trial_counter': current_trial_counter
-                            },
-                            f,
+
+                succesful_trials = []
+                for i in range(current_i, NUM_SENTENCES):
+                    sentence = all_sentences[i]["sentence"]
+                    dataset = all_sentences[i]["dataset"]
+
+                    try:
+                        evaluation_score = evaluate_sentence(
+                            evaluation_prompt=evaluation_prompt,
+                            sentence=sentence,
+                            temperature=temperature_value,
+                            model=model,
                         )
-                    raise e
+                    except Exception as e:
+                        # save the current state before terminating
+                        with open("last_successful_state.json", "w") as f:
+                            json.dump(
+                                {
+                                    "subject": current_subject,
+                                    "temperature_type": current_temperature_type,
+                                    "evaluation_type": current_evaluation_type,
+                                    "trial_counter": current_trial_counter,
+                                },
+                                f,
+                            )
 
-                separated_sentences_results = list(
-                    map(
-                        lambda score_sentence: assign_experiments_values(
-                            score=score_sentence[0],
-                            temperature=temperature_type,
-                            sentence=score_sentence[1]["sentence"],
-                            evaluation_prompt=evaluation_type,
-                            dataset=score_sentence[1]["dataset"],
-                            subject=subject,
-                            trial=trial_counter + score_sentence[2],
-                        ),
-                        zip(evaluation_scores, all_sentences, range(NUM_SENTENCES)),
+                        # save advances before terminating
+                        print("Encountered and error, saving progress...")
+                        df = pd.concat(
+                            [df, pd.DataFrame(succesful_trials)], ignore_index=True
+                        )
+                        df.to_csv(output_file, index=False)
+                        raise e
+
+                    # store progress
+                    trial_results = assign_experiment_values(
+                        score=evaluation_score,
+                        temperature=temperature_type,
+                        sentence=sentence,
+                        evaluation_prompt=evaluation_type,
+                        dataset=dataset,
+                        subject=subject,
+                        trial=trial_counter,
                     )
-                )
-                trial_counter += NUM_SENTENCES
+                    succesful_trials.append(trial_results)
 
-                # create new df concatenating experiment results
-                df = pd.concat(
-                    [df, pd.DataFrame(separated_sentences_results)], ignore_index=True
-                )
+                    # update the current state
+                    trial_counter += 1
+                    current_subject = subject
+                    current_temperature_type = temperature_type
+                    current_evaluation_type = evaluation_type
+                    current_trial_counter = trial_counter
+                    progress_bar.update(1)
 
-                # save df preemptively (in case OpenAI fails us)
+                # save advances after succesful iteration
+                df = pd.concat([df, pd.DataFrame(succesful_trials)], ignore_index=True)
                 df.to_csv(output_file, index=False)
-
-                # Update the current state
-                current_subject = subject
-                current_temperature_type = temperature_type
-                current_evaluation_type = evaluation_type
-                current_trial_counter = trial_counter
 
     # delete the state file when run completes
     if "last_successful_state.json" in os.listdir():
         os.remove("last_successful_state.json")
 
 
+def run_with_retries(
+    model: str = "gpt-3.5",
+    output_file: str = "results.csv",
+    delay_seconds: int = 5,
+    max_retries: int = 5,
+) -> None:
+    original_delay_seconds = delay_seconds
+    consecutive_failures = 0
+    last_exception_message = None
+
+    while True:
+        try:
+            rate_sentences(model, output_file)
+            break
+        except Exception as e:
+            if str(e) == last_exception_message:
+                consecutive_failures += 1
+                delay_seconds += delay_seconds
+            else:
+                last_exception_message = str(e)
+                consecutive_failures = 1
+                delay_seconds = original_delay_seconds
+
+            print(f"Exception encountered: {e}")
+            print(traceback.format_exc())
+
+            if consecutive_failures >= max_retries:
+                raise RuntimeError(
+                    """
+                    Same exception occurred for the maximum number of retries, \
+                    terminating process.
+                    """
+                )
+
+            if consecutive_failures > 0:
+                print("Encountered same exception. Extending delay time...")
+
+            print(f"Retrying in {delay_seconds} seconds...")
+            time.sleep(delay_seconds)
+
+
 if __name__ == "__main__":
-    fire.Fire(rate_sentences)
+    fire.Fire(run_with_retries)
